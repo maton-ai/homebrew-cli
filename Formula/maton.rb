43367a754d363453b76ac11247873eb3ec45c45a
maton automation <noreply@maton.ai>
2026-09-23 08:33:17 +0000

    maton 0.3.3

--- Formula/maton.rb
+++ Formula/maton.rb
@@ -1,16 +1,16 @@
 class Maton < Formula
   desc "Maton's official command line tool"
   homepage "https://maton.ai"
-  version "0.3.2"
+  version "0.3.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/maton-ai/cli/releases/download/v0.3.2/maton_0.3.2_macOS_arm64.zip"
-      sha256 "063bb51ced8c60703edd81bd336320e7bdc623f7b49a15191bd63815d74374a3"
+      url "https://github.com/maton-ai/cli/releases/download/v0.3.3/maton_0.3.3_macOS_arm64.zip"
+      sha256 "85afcdc8874944ac578f2261928ffad4c2fba64cbffba1e6ce30ccc2a7392910"
     end
     on_intel do
-      url "https://github.com/maton-ai/cli/releases/download/v0.3.2/maton_0.3.2_macOS_amd64.zip"
-      sha256 "53fad55d6329502281c1ca9d25ca0b2675f833fd68f0b84a20048ea6b6c70e00"
+      url "https://github.com/maton-ai/cli/releases/download/v0.3.3/maton_0.3.3_macOS_amd64.zip"
+      sha256 "64036e8ba6d7ab63e991b079a8097aa7e7ed22a00a007e5ffacfd6eec2596d20"
     end
   end
 
